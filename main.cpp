ae6740ff38bd85c3dc55e1bdafb868ac492bcf51
kintrix007 <boldizsar02@gmail.com>
2023-05-29 14:42:22 +0200

    feat: Got started implementing the "db file"

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "src/arguments.h"
 #include "src/database.h"
 #include "src/generator.h"
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -17,7 +18,15 @@ int main(int argc, char **argv) {
       .symbols = false,
   };
 
+
   Database::DB *db = new Database::DB;
+  
+  std::ifstream in("test.db");
+  in >> db;
+  in.close();
+  
+  std::cout << db;
+
   // Database::add_password(db, Database::new_entry (
   //       "new password",
   //       "um actually...",
@@ -27,13 +36,13 @@ int main(int argc, char **argv) {
   // std::cout << Database::entry_exists(db, "name") << std::endl;
   // std::cout << Database::entry_exists(db, "name2") << std::endl;
 
-  std::cin >> db;
-  std::cout << db << std::endl;
-  
   // std::cout << (*Database::get_entries(db)[0]->name) << std::endl;
 
   // std::string password = Password::generate(opts);
   // std::cout << password << std::endl;
+  std::ofstream out("test.db");
+  out << db;
+  out.close();
 
   return 0;
 }
