0a83e447d9bd5f995fa15ceabdcda03ec781cd30
kintrix007 <boldizsar02@gmail.com>
2023-05-29 11:40:49 +0200

    Added template for argument parser

---
+++ src/arguments.cpp
@@ -0,0 +1,12 @@
+#include "src/arguments.h"
+
+Args::Command::~Command() { delete this->flags; }
+
+Args::FlagsUnion::~FlagsUnion() {}
+
+const Args::Command Args::parse(int argc, char **argv) {
+  FlagsUnion *flags = new FlagsUnion{};
+  flags->help = {};
+  Args::Command command = {HELP, flags};
+  return command;
+}
---
+++ src/arguments.h
@@ -0,0 +1,52 @@
+#ifndef ARGUMENTS_H
+#define ARGUMENTS_H
+
+#include <vector>
+namespace Args {
+enum Mode { HELP, ADD, EDIT, REMOVE, SEARCH, LIST };
+enum Field { NAME, CATEGORY, WEBSITE, USERNAME };
+
+struct HelpFlags {};
+
+struct AddFlags {
+  bool gen_lower;
+  bool gen_upper;
+  bool gen_number;
+  bool gen_symbol;
+};
+
+struct EditFlags {};
+
+struct RemoveFlags {
+  bool batch;
+};
+
+struct SearchFlags {
+  Field search_on;
+};
+
+struct ListFlags {
+  std::vector<Field> sort_on;
+};
+
+union FlagsUnion {
+  HelpFlags help;
+  AddFlags add;
+  EditFlags edit;
+  RemoveFlags remove;
+  SearchFlags search;
+  ListFlags list{{}};
+
+  ~FlagsUnion();
+};
+
+struct Command {
+  Mode mode;
+  FlagsUnion *flags;
+  ~Command();
+};
+
+const Command parse(int argc, char **argv);
+} // namespace Args
+
+#endif
