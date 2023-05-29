c3bbe4b62d7e813845cd4227d1ee9780c27d3e91
kintrix007 <boldizsar02@gmail.com>
2023-05-29 08:57:01 +0200

    feat: Implemented most of the basic features of db

--- src/database.cpp
+++ src/database.cpp
@@ -1,3 +1,66 @@
 #include "src/database.h"
+#include <optional>
+#include <vector>
 
+const std::vector<Database::Entry *> Database::get_entries(const DB *db) {
+  std::vector<Database::Entry *> entries;
 
+  for (const auto &[_, entry] : *db) {
+    entries.push_back(entry);
+  }
+
+  return entries;
+}
+
+bool Database::entry_exists(const DB *db, const std::string &entry_name) {
+  return db->find(entry_name) == db->end();
+}
+
+std::optional<const Database::Entry *>
+Database::get_entry_by_name(const DB *db, const std::string &entry_name) {
+  if (!entry_exists(db, entry_name)) {
+    return {};
+  }
+  return db->at(entry_name);
+}
+
+bool Database::add_password(DB *db, Entry *entry) {
+  return db->insert({*entry->name, entry}).second;
+}
+
+void Database::update_password(DB *db, const std::string &entry_name,
+                               const Entry *new_entry) {
+  auto entry = db->at(entry_name);
+
+  if (entry->name != new_entry->name) {
+    entry->name = new_entry->name;
+    db->erase(entry_name);
+    db->insert({*entry->name, entry});
+  }
+
+  if (entry->password != new_entry->password) {
+    entry->password = new_entry->password;
+  }
+
+  if (entry->category != new_entry->category) {
+    entry->category = new_entry->category;
+  }
+
+  if (entry->username != new_entry->username) {
+    entry->username = new_entry->username;
+  }
+
+  if (entry->website != new_entry->website) {
+    entry->website = new_entry->website;
+  }
+}
+
+bool Database::delete_password(DB *db, const std::string &entry_name) {
+  if (!entry_exists(db, entry_name)) {
+    return false;
+  }
+
+  delete db->at(entry_name);
+  db->erase(entry_name);
+  return true;
+}
--- src/database.h
+++ src/database.h
@@ -2,17 +2,50 @@
 #define DATABASE_H
 
 #include <cstdint>
-#include <string>
 #include <optional>
-#include <functional>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace Database {
-    struct Entry {
-        std::string name;
-        std::string password;
-        uint64_t category;
-        std::optional<std::string> website;
-    };
-}
+/**
+ * The name has to be globally unique.
+ */
+struct Entry {
+  std::string *name;
+  std::string *password;
+  uint64_t category;
+  std::optional<const std::string> *username;
+  std::optional<const std::string> *website;
+
+  ~Entry() {
+    delete this->name;
+    delete this->password;
+    delete this->username;
+    delete this->website;
+  }
+};
+
+typedef std::unordered_map<std::string, Entry *> DB;
+
+const std::vector<Entry *> get_entries(const DB *db);
+
+inline bool entry_exists(const DB *db, const std::string &entry_name);
+
+std::optional<const Entry *> get_entry_by_name(const DB *db,
+                                               const std::string &entry_name);
+
+inline bool add_password(DB *db, Entry *entry);
+
+/*
+ * Instead of replacing the previous entry with the new one,
+ * copy the given contents of the given enrty into the pre-existing one
+ * due to completely arbitrary functional requrements...
+ */
+void update_password(DB *db, const std::string &entry_name,
+                     const Entry *new_entry);
+
+bool delete_password(DB *db, const std::string &entry_name);
+} // namespace Database
 
 #endif
